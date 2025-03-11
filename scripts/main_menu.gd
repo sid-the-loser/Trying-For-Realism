df990f3dbb976355013a434d0ceb65e8406d5c18
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-11 00:01:54 -0400

    connected all main menu buttons and added quitting

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
