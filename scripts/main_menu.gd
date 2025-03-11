1c558893a85a1ddcf1ad9d6006ab3de6ede3a5a3
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-11 01:51:41 -0400

    updated play button functionality

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func _on_options_button_pressed() -> void:
