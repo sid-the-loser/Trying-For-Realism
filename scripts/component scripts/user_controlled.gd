be5e6a4f5e2b8b35e2a8e3789350fc74b472c7a3
Sidharth Suresh <sidharthsuresh07@gmail.com>
2025-03-19 01:21:28 -0400

    added some base variables to the user control component

--- scripts/component scripts/user_controlled.gd
+++ scripts/component scripts/user_controlled.gd
@@ -1,3 +1,5 @@
+# Code referenced from: https://youtu.be/xIKErMgJ1Yk
+
 extends Node
 class_name UserControlled
 
@@ -5,4 +7,18 @@ class_name UserControlled
 @export var character_body: CharacterBody3D
 @export var head: Node3D
 
-# TODO: do the rest
+@export_category("Movement related variables")
+@export var JUMP_VELOCITY = 4.5
+@export var WALKING_SPEED = 8.0
+@export var CROUCH_DEPTH = 0.5
+@export var MOUSE_SENSE = 0.4
+@export var LERP_SPEED = 10.0
+
+var current_speed = 8.0
+var direction = Vector3.ZERO
+var head_will_collide = false
+var can_move = false
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
